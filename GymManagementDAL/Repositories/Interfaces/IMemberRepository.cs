81d77edb6b638470d982b508a5a42c3fac6384d2
ahmed <ahmed@AHMED_SAMY>
2025-10-08 23:25:17 +0300

    FinalDemo

---
+++ GymManagementDAL/Repositories/Interfaces/IMemberRepository.cs
@@ -0,0 +1,34 @@
+﻿using GymManagementDAL.Data.Contexts;
+using GymManagementDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Repositories.Interfaces
+{
+    internal interface IMemberRepository
+    {
+        
+        
+        
+        // GetAll
+        IEnumerable<Member> GetAll();
+        // GetById
+        Member? GetById(int id);
+        // Add
+        int Add(Member member);
+        // Update
+        int Update(Member member);
+        // Delete
+        int Delete(int id);
+
+
+
+
+
+
+
+    }
+}
